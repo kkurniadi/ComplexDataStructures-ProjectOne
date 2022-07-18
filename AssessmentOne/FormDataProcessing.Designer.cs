3f627cfbc2185876ef585f61e107ecda3e373ca4
kkurniadi <90660898+kkurniadi@users.noreply.github.com>
2022-07-18 13:53:28 +0800

    Add NumericUpDown controls

--- AssessmentOne/FormDataProcessing.Designer.cs
+++ AssessmentOne/FormDataProcessing.Designer.cs
@@ -28,20 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.numericUpDownSigma = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownMu = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSigma)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMu)).BeginInit();
             this.SuspendLayout();
+            // 
+            // numericUpDownSigma
+            // 
+            this.numericUpDownSigma.Location = new System.Drawing.Point(42, 39);
+            this.numericUpDownSigma.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDownSigma.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownSigma.Name = "numericUpDownSigma";
+            this.numericUpDownSigma.Size = new System.Drawing.Size(47, 20);
+            this.numericUpDownSigma.TabIndex = 0;
+            this.numericUpDownSigma.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sigma";
+            // 
+            // numericUpDownMu
+            // 
+            this.numericUpDownMu.Location = new System.Drawing.Point(107, 39);
+            this.numericUpDownMu.Maximum = new decimal(new int[] {
+            75,
+            0,
+            0,
+            0});
+            this.numericUpDownMu.Minimum = new decimal(new int[] {
+            35,
+            0,
+            0,
+            0});
+            this.numericUpDownMu.Name = "numericUpDownMu";
+            this.numericUpDownMu.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDownMu.TabIndex = 2;
+            this.numericUpDownMu.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mu";
             // 
             // FormDataProcessing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownMu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownSigma);
             this.Name = "FormDataProcessing";
             this.Text = "Data Processing";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSigma)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMu)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownSigma;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownMu;
+        private System.Windows.Forms.Label label2;
     }
 }
 
